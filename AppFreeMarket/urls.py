857a6951d040295c300b64cf12d22aace1550411
Patricia Isbert <patrisbert@gmail.com>
2023-03-09 21:20:50 -0300

    Working

--- AppFreeMarket/urls.py
+++ AppFreeMarket/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from AppFreeMarket import views
 
-urlpatterns = {
+urlpatterns = [
     path('', views.home, name='Home'), 
     path('buyer', views.buyer, name='Buyer'),
     path('seller', views.seller, name='Seller'),
     path('delivery', views.delivery, name='Delivery'),
     path('searchDelivery', views.searchDelivery, name='SearchDelivery'),
     path('search/', views.search),
-}
+]
